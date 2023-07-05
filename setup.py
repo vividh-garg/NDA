e9d3b2f86a95efc540e31bf36f85c43826667cd1
vividh-garg <vividh98@gmail.com>
2023-07-05 16:32:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='nda_tools',
     version=VERSION,
-    author='Mike Huls',
-    author_email='mike_huls@hotmail.com',
+    author='Vividh Garg',
+    author_email='vividhgarg@hotmail.com',
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
