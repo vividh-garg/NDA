dbbcc7f27440fedbb25f5e8e72b6298eaf7a0342
vividh-garg <vividh98@gmail.com>
2023-07-05 17:03:48 +0530

    Update __init__.py

--- nda_tools/__init__.py
+++ nda_tools/__init__.py
@@ -1,8 +1,7 @@
-import nda_functions
-from nda_functions import get_process_name
-from nda_functions import get_barcode
-from nda_functions import get_start_time
-from nda_functions import cycle
-from nda_functions import recipe
-from nda_functions import step
-from nda_functions import records
+from .nda_functions import get_process_name
+from .nda_functions import get_barcode
+from .nda_functions import get_start_time
+from .nda_functions import cycle
+from .nda_functions import recipe
+from .nda_functions import step
+from .nda_functions import records
